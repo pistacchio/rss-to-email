efaa5641252be84539bc75199473e08b67c8c2a9
pistacchio <pistacchioso@proton.me>
2024-03-20 10:20:58 +0100

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,4 +1,5 @@
 export const feeds = [
   'https://godotengine.org/rss.xml',
-  'https://dbushell.com/rss.xml'
+  'https://dbushell.com/rss.xml',
+  'https://kagifeedback.org/atom/t/release-notes'
 ]
